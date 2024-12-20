0a6b0f143b7a9b8c60eed1e9fb9a9f1c74c4a0ce
ultimathule1 <ranel28021998@gmail.com>
2024-12-20 09:43:02 +0300

    First commit

---
+++ src/main/java/dev/mvc_users_pets_project/MvcUsersPetsProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.mvc_users_pets_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcUsersPetsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcUsersPetsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mvc_users_pets_project/MvcUsersPetsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mvc_users_pets_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcUsersPetsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
