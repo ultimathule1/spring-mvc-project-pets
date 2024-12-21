f0e374fb733887b832e23dfdd11213d4d94053af
ultimathule1 <ranel28021998@gmail.com>
2024-12-22 00:13:02 +0300

    GlobalExceptionHandler is ready

--- src/main/java/dev/mvc_users_pets_project/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/mvc_users_pets_project/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,88 @@
 package dev.mvc_users_pets_project.exceptions;
 
+import dev.mvc_users_pets_project.model.ErrorMessageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessageResponse> handleException(Exception ex) {
+        log.error("Received an error ", ex);
+
+        var errorMessageResponse = new ErrorMessageResponse(
+                "Server Error",
+                ex.getMessage(),
+                LocalDateTime.now());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorMessageResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessageResponse>handleValidationException(MethodArgumentNotValidException ex){
+        log.error("Received a validation error", ex);
+
+        String detailedMessage = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError ->
+                    fieldError.getField() + ": " + fieldError.getDefaultMessage()
+                )
+                .collect(Collectors.joining(", "));
+
+        var errorMessageResponse = new ErrorMessageResponse(
+                "Validation error. Incorrect value!",
+                detailedMessage,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessageResponse);
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<ErrorMessageResponse> handleIllegalArgumentException(IllegalArgumentException ex){
+        log.error("Received an illegal argument", ex);
+
+        var errorMessageResponse = new ErrorMessageResponse(
+                "Illegal error.",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorMessageResponse);
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<ErrorMessageResponse> handleNoSuchElementException(NoSuchElementException ex){
+        log.error("Received an element not found", ex);
+
+        var errorMessageResponse = new ErrorMessageResponse(
+                "No such element not found.",
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorMessageResponse);
+    }
 }
---
+++ src/main/java/dev/mvc_users_pets_project/model/ErrorMessageResponse.java
@@ -0,0 +1,10 @@
+package dev.mvc_users_pets_project.model;
+
+import java.time.LocalDateTime;
+
+public record ErrorMessageResponse(
+    String message,
+    String detailMessage,
+    LocalDateTime dateTime
+) {
+}
